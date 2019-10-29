7ec548d43538f774755e30ade38d1d14fea96c02
LUIZA DE PAULA ANDRES <luiza.andres@alunos.sc.senac.br>
2019-10-28 21:09:13 -0300

    comecei a main

--- Locacao.java
+++ Locacao.java
@@ -1,10 +1,40 @@
+import java.sql.Date;
+import java.util.List;
+
+
 public class Locacao{
-    int id_Locacao;
+    int id_Loc;
     Cliente id_Cliente;
     Cliente nome_Cliente;
     String dt_Loc;
     String dt_Dev;
     Float valor_Tot;
-    
+    //criando lista de filmes do carrinho
+    List<Filme> filmes = new ArrayList<>();
+    Date data=new Date();
 
+    //criando o aluguel
+    void Alugar(int id_Loc, Cliente id_Cliente, Cliente nome_Cliente){
+        this.id_Loc = id_Loc;
+        this.id_Cliente = id_Cliente;
+        this.nome_Cliente= nome_Cliente;
+        this.dt_Loc = data;
+    }
+    //retorna id da locação
+    int Id_Locacao() {
+        return id_Loc;
+    }
+    //adidiona filme na lista
+    void addFilme(Filme filme){
+        this.filmes.add(filme);
+    }
+    float calcValorTot(){
+        for(Filme filme: filmes){
+            this.valor_Tot = this.valor_Tot + filme.valor;
+        }
+        return valor_Tot;
+    }
+    void dataDev(){
+        int dias = Cliente.dias_Dev;
+    }
 }
