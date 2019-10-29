1c3854a1331a0ca71813b16a2d9d47e74999f358
LUIZA DE PAULA ANDRES <luiza.andres@alunos.sc.senac.br>
2019-10-28 22:13:48 -0300

    é o que deu pra hj, segue o jogo

--- Blockbuster.java
+++ Blockbuster.java
@@ -1,12 +1,12 @@
 public class Blockbuster{
     public static void main(String[] args) {
-
         //criando os clientes
-        Cliente cliente1 = new Cliente(1, "Nome 1", "01/01/2001", "123.456.789-10", 2){
-        cliente1.dadosCLiente(1);
-    }
+        Cliente cliente1 = new Cliente(1, "Nome 1", "01/01/2001", "123.456.789-10", 2);
+        cliente1.dadosCliente();
         Cliente cliente2 = new Cliente(2, "Nome 2", "02/02/2002", "234.567.891-01", 2);
+        cliente2.dadosCliente();
         Cliente cliente3 = new Cliente(3, "Nome 3", "03/03/2003", "345.678.910-11", 2);
+        cliente3.dadosCliente();
         Cliente cliente4 = new Cliente(4, "Nome 4", "04/04/2004", "456.789.101-11", 2);
         Cliente cliente5 = new Cliente(5, "Nome 5", "05/05/2005", "567.891.011-12", 2);
         Cliente cliente6 = new Cliente(6, "Nome 6", "06/06/2006", "678.910.111.21", 2);
@@ -14,16 +14,27 @@ public class Blockbuster{
         Cliente cliente8 = new Cliente(8, "Nome 8", "08/08/2008", "891.011.121-31", 2);
         Cliente cliente9 = new Cliente(9, "Nome 9", "09/09/2009", "910.111.213-14", 2);
         Cliente cliente10 = new Cliente(10, "Nome 10", "10/10/2010", "101.112.131.41", 2);
-/*        //criar os filmes
-        Filme filme1 = new Filme(1, "Almoço", 01/01/1990, "Comer, comer...", 8.00, 5);
-        Filme filme2 = new Filme(2, "Banana", 01/01/1990, "...comer, comer", 7.00, 6);
-        Filme filme3 = new Filme(3, "Churrasco", 01/01/1990, "Fominha", 9.00, 7);
-        Filme filme4 = new Filme(4, "Damasco", 01/01/1990, "Não jantei ainda", 12.10, 4);
-        Filme filme5 = new Filme(5, "Espinafre", 01/01/1990, "E almocei muito cedo", 4.50, 6);
-        Filme filme6 = new Filme(6, "Farofa", 01/01/1990, "agora meu estomago ta roncando", 6.80, 3);
-        Filme filme7 = new Filme(7, "Guaraná", 01/01/1990, "e eu não consigo não pensar em comida", 4.20, 1);
-        Filme filme8 = new Filme(8, "Joelho de Porco", 01/01/1990, "Bem quentinha", 6.30, 7);
-        Filme filme9 = new Filme(9, "Linguica", 01/01/1990, "Pra esquentar", 2.80, 4);
-        Filme filme10 = new Filme(10, "Macarrão", 01/01/1990, "Minha barriguinha", 7.50, 3);*/
+        //criar os filmes
+        Filme filme1 = new Filme(1, "Almoço", "01/01/1990", "Comer, comer...", 8.00, 5);
+        
+        Filme filme2 = new Filme(2, "Banana", "01/01/1990", "...comer, comer", 7.00, 6);
+        Filme filme3 = new Filme(3, "Churrasco", "01/01/1990", "Fominha", 9.00, 7);
+        Filme filme4 = new Filme(4, "Damasco", "01/01/1990", "Não jantei ainda", 12.10, 4);
+        Filme filme5 = new Filme(5, "Espinafre", "01/01/1990", "E almocei muito cedo", 4.50, 6);
+        Filme filme6 = new Filme(6, "Farofa", "01/01/1990", "agora meu estomago ta roncando", 6.80, 3);
+        Filme filme7 = new Filme(7, "Guaraná", "01/01/1990", "e eu não consigo não pensar em comida", 4.20, 1);
+        Filme filme8 = new Filme(8, "Joelho de Porco", "01/01/1990", "Bem quentinha", 6.30, 7);
+        Filme filme9 = new Filme(9, "Linguica", "01/01/1990", "Pra esquentar", 2.80, 4);
+        Filme filme10 = new Filme(10, "Macarrão", "01/01/1990", "Minha barriguinha", 7.50, 3);
+        //criar locação
+        Locacao locacao1 = new Locacao(1, cliente1);
+        locacao1.AddFilme(filme1);
+        locacao1.AddFilme(filme2);
+        //valor da locacao
+       /* locacao1.calcValorTot(){
+            for(Filme filme: filmes){
+                locacao1.valorTot = locacao1.valorTot + filme.valor;
+            }
+        }  */
     }
 }
