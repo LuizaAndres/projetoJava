0cb74837296c3a592fb60b3abeaa5d57a072d850
LUIZA DE PAULA ANDRES <luiza.andres@alunos.sc.senac.br>
2019-12-03 20:32:41 -0300

    ta arrumando, finalmente

--- DAO.java
+++ DAO.java
@@ -1,56 +1,72 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import com.mysql.cj.xdevapi.PreparableStatement;
-
-import Conexao;
 
 public class DAO {
     private Connection connection;
-    int idPessoa;
-    String nomePessoa;
-    String emailPessoa;
-    double percDesconto;
-    double imposto;
-    int idConta;
-    int mes;
-    int ano;
-    double valor;
 
-    public DAO() {
-        this.connection = new Connection().getConnection();
+    public int insert(String sql, Object[] atribs) throws Exception {
+        try{
+            PreparedStatement statement = this.startConnection().prepareStatement(
+                sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            // Atribui os valores
+            insertAtribs(statement, atribs);
+
+            // Verifica se o retorno é um valor criado
+            if(statement.executeUpdate() > 0){
+                ResultSet resultado = statement.getGeneratedKeys();
+
+                if(resultado.next()){
+                    return resultado.getInt(1);
+                }
+            }
+
+            return -1;
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    public void adicionaPessoa(Pessoas pessoas) {
-        String sql = "INSERT INTO pessoas (idPessoa,nomePessoa,EmailPessoa) VALUES (?,?,?)";
-        try {
-            PreparableStatement stmt = connection.prepareStatement(sql);
-            stmt.setInt(1, pessoas.getId());
-            stmt.setString(2, pessoas.getNome());
-            stmt.setString(3, pessoas.getEmail());
-        } catch (SQLException u) {
-            throw new RuntimeException(u);
+    private void insertAtribs(PreparedStatement statement, Object[] atribs) throws SQLException {
+        int idx = 1;
+        for(Object atrib: atribs){
+            if(atrib instanceof String) {
+                statement.setString(idx, (String) atrib);
+            } else if (atrib instanceof Integer){
+                statement.setInt(idx, (Integer) atrib);
+            }
+            idx++;
         }
-        String sql1 = "INSERT INTO proventos (idPessoa,imposto,valor,mes,ano) VALUES (?,?,?,?,?)";
-        try {
-            PreparableStatement stmt = connection.prepareStatement(sql1);
-            stmt.setInt(proventos.getId());
-            stmt.setDouble(proventos.getImposto());
-            stmt.setDouble(proventos.getValor());
-            stmt.setInt(proventos.getMes());
-            stmt.setInt(proventos.getAno());
-        } catch (SQLException u) {
-            throw new RuntimeException(u);
+    }
+
+    private Pessoas getPessoas(PreparedStatement statement, Object[] atribs) throws SQLException{
+        String sql = "SELECT * FROM pessoas";
+        Statement statemente = conexao.createStatement();
+        ResultSet resultados = statemente.executeQuery(sql);
+
+        while(resultados.next()){
+            Usuario user = new Usuario(
+                resultados.getInt("id"),
+                resultados.getString("user_name"),
+                resultados.getString("password"),
+                resultados.getString("name")
+            );
+    }
+
+
+    private Connection startConnection() throws Exception {
+        if(this.connection != null && !connection.isClosed()){
+            return this.connection;
         }
-        String sql2 = "INSERT INTO dividas (idPessoa,desconto,valor,mes,ano) VALUES (?,?,?,?,?)";
-        try {
-            PreparableStatement stmt = connection.prepareStatement(sql2);
-            stmt.setInt(dividas.getIdPessoa());
-            stmt.setDouble(dividas.getDesconto());
-            stmt.setDouble(dividas.getValor());
-            stmt.setInt(dividas.getMes());
-            stmt.setInt(dividas.getAno());
-        } catch (SQLException u) {
-            throw new RuntimeException(u);
-        }
+
+        this.connection = Conexao.getConnection();
+
+        return this.connection;
+    }
+
+    public void endConnection() throws SQLException, Exception {
+        startConnection().close();
     }
 }
